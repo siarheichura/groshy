10a16eeb5dc268314e9ce7fbaaa5807551155acf
siarheichura <chura.siarhei@gmail.com>
2022-01-18 12:06:58 +0300

    add basic routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
+import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { StatisticsPageComponent } from './statistics-page/statistics-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LoginPageComponent } from './login-page/login-page.component';
 
-const routes: Routes = [{ path: 'login', component: LoginPageComponent }];
+const routes: Routes = [
+  { path: '', component: MainPageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'profile', component: ProfilePageComponent },
+  { path: 'statistics', component: StatisticsPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

a94595e90972b571835e14bfd4bdb434411a2421
siarheichura <chura.siarhei@gmail.com>
2022-01-26 12:17:10 +0300

    edit router structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,8 @@
+import { UserAmountComponent } from './shared/user-amount/user-amount.component';
+import { StatisticsPageComponent } from './components/statistics-page/statistics-page.component';
+import { MoneymovePageComponent } from './components/moneymove-page/moneymove-page.component';
+import { HomePageModule } from './components/home-page/home-page.module';
+import { HomePageComponent } from './components/home-page/home-page.component';
 import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -8,6 +13,23 @@ import { RouterEnum } from './shared/enums/RouterEnum';
 
 const routes: Routes = [
   { path: RouterEnum.Login, component: LoginPageComponent },
+
+  {
+    path: RouterEnum.Index,
+    component: MainLayoutComponent,
+    children: [
+      { path: RouterEnum.Index, component: HomePageComponent },
+      {
+        path: `${RouterEnum.Wallet}/:id`,
+        component: UserAmountComponent,
+        children: [
+          { path: `${RouterEnum.Wallet}/:id`, redirectTo: '' },
+          { path: RouterEnum.Index, component: MoneymovePageComponent },
+          { path: RouterEnum.Statistics, component: StatisticsPageComponent },
+        ],
+      },
+    ],
+  },
 ];
 
 @NgModule({
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { NzSpinModule } from 'ng-zorro-antd/spin';
-
 @NgModule({
   declarations: [],
-  imports: [CommonModule, NzSpinModule],
+  imports: [CommonModule],
   providers: [],
 })
 export class SharedModule {}

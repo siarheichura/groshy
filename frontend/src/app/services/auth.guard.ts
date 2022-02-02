f22b35c0dbba04980b2d2fbb7039a8a32b168848
siarheichura <chura.siarhei@gmail.com>
2022-02-02 14:14:55 +0300

    add routes protection without login

---
+++ frontend/src/app/services/auth.guard.ts
@@ -0,0 +1,19 @@
+import { AuthService } from './auth.service';
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+
+import { RouterEnum } from 'src/app/shared/enums/RouterEnum';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(): boolean {
+    if (this.authService.isAuthenticated()) {
+      return true;
+    } else {
+      this.router.navigate([RouterEnum.Auth]);
+      return false;
+    }
+  }
+}
